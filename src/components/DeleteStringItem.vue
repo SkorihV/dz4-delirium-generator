980f2eea3ce3eadd48db138f00495537fc22d64e
SkorihV <skorih.vitalii@gmail.com>
2020-06-02 15:47:21 +0700

    Добавил кнопку удаления строчки через компоненту

---
+++ src/components/DeleteStringItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="deleteString" :indexBtn="indexDelete">Удалить строчку</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    deleteString() {
+      this.$emit("deleteString", this.indexDelete);
+    }
+  },
+  props: {
+    indexDelete: Number
+  }
+};
+</script>
--- src/components/StringsArr.vue
+++ src/components/StringsArr.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="stringItemsList">
-    <div class="stringItem" v-for="(item, index) in stringList" :key="index">{{item}}</div>
+    <div class="stringItem" v-for="(item, index) in stringList" :key="index">
+      <div>{{ item }}</div>
+      <span><delete @deleteString="deleteItem(index)" :indexDelete="index"></delete></span>
+    </div>
   </div>
 </template>
 
 <script>
+import DeleteString from "./DeleteStringItem.vue";
+
 export default {
   data: () => ({
     stringList: []
   }),
+  methods: {
+    deleteItem(index) {
+      this.stringList.splice(index, 1);
+    }
+  },
   props: {
     stringForShow: String
   },
@@ -16,6 +26,9 @@ export default {
     stringForShow() {
       this.stringList.push(this.stringForShow);
     }
+  },
+  components: {
+    delete: DeleteString
   }
 };
 </script>
@@ -25,6 +38,8 @@ export default {
   padding: 10px;
   margin: 5px;
   border-right: 2px solid green;
+  display: flex;
+  justify-content: space-between;
 }
 .stringItemsList {
   flex-grow: 1;
